065286c2c6fd999e74d8cb7367001f8eb4dacfa3
Fernando Muñoz <fernando.javier.munoz1@hotmail.com>
2025-04-20 20:34:10 -0400

    Catálogo de compras y posibilidad de comprar funcionales + algunos comments para facilidad de lectura

--- funcionesCliente.py
+++ funcionesCliente.py
@@ -29,23 +29,26 @@ class compra:
     def asdict(self):
         return {"nombre":self.name, "fecha":self.date, "precio":self.price, "pago":self.payStatus, "envio":self.shipStatus, "recib":self.recv, "dev":self.dev}
 
-mutex = threading.Lock()
+def datetoDict(date):
+    return {"año":date.year, "mes":date.month, "dia":date.day, "hora":date.time().hour, "minuto":date.time().minute}
 
-def cambioContraseña(sock, filepath, mail):
+mutex = threading.Lock() 
+
+def cambioContraseña(sock, filepath, mail): # cambiar la contraseña actual de un usuario
     while True:
         sock.sendall("Ingrese su contraseña actual: ".encode())
         act = sock.recv(1024).decode()
         with mutex:
-            with open(filepath, "r+") as file:
+            with open(filepath, "r+") as file: # se abre el archivo asociado a los clientes
                 data = json.load(file)
-                if act == data[mail][0]:
+                if act == data[mail][0]: # si la contraseña ingresada coincide con la registrada...
                     while True:
-                        sock.sendall("Ingrese su nueva contraseña: ".encode())
-                        new = sock.recv(1024).decode()
+                        sock.sendall("Ingrese su nueva contraseña: ".encode()) 
+                        new = sock.recv(1024).decode() # se recibe la contraseña nueva
                         sock.sendall("Repita su nueva contraseña: ".encode())
-                        newRep = sock.recv(1024).decode()
-                        if new == newRep:
-                            data[mail][0] = new
+                        newRep = sock.recv(1024).decode() # se recibe una confirmación de la contraseña nueva
+                        if new == newRep: # si las contraseñas coinciden...
+                            data[mail][0] = new # se setea la contraseña ingresada cómo la nueva contraseña
                             file.seek(0)
                             json.dump(data, file, indent = 4)
                             file.truncate()
@@ -54,53 +57,54 @@ def cambioContraseña(sock, filepath, mail):
                             print(f"[SERVIDOR]: Cambio de clave - Cliente {data[mail][1]}")
                             break
                         else:
-                            sock.sendall("Las contraseñas no coinciden, intente nuevamente.".encode())
+                            sock.sendall("Las contraseñas no coinciden, intente nuevamente.".encode()) # si la nueva contraseña y su confirmación no coincide
                     break
                 else:
-                    sock.sendall("Contraseña incorrecta, intente nuevamente.".encode())
+                    sock.sendall("Contraseña incorrecta, intente nuevamente.".encode()) # si la contraseña ingresada no coincide con la registrada en la base de datos
     return None
 
-def catalogoCompra(sock, filepath1, filepath2, mail):
+def catalogoCompra(sock, filepath1, filepath2, mail): # ver el catálogo de la tienda y permitir comprar artículos (se podría agregar comprar más de una unidad por acción)
     while True:
         with mutex:
-            with open(filepath1, "r") as file1:
+            with open(filepath1, "r+") as file1: # se abre el archivo JSON con los artículos
                 data1 = json.load(file1)
                 for key, value in data1.items():
-                    sock.sendall(f"[{key}] {value[0]} - ${value[1]}\n".encode())
+                    sock.sendall(f"[{key}] {value[0]} - ${value[1]}\n".encode()) # se muestran los elementos del catálogo
                 sock.sendall("¿Desea comprar algún elemento del catálogo? Ingrese un número (0 = Salir)".encode())
                 ans = sock.recv(1024).decode()
-                if ans == '0':
+                if ans == '0': # salir del catálogo sin comprar nada
                     break
-                elif ans.isnumeric() and 0 < int(ans) < len(data1) + 1:
+                elif ans.isnumeric() and 0 < int(ans) < len(data1) + 1: # si el artículo es válido...
                     sock.sendall(f"Confirme la compra de '{data1[ans][0].lower()}' por {data1[ans][1]} (1 = Confirmar - 0 = Cancelar)".encode())
                     conf = sock.recv(1024).decode()
-                    if conf == "1":
-                        if data1[ans][2] > 0:
-                            with mutex:
-                                with open(filepath2, "r+") as file2:
-                                    data2 = json.load(file2)
-                                    data2[mail][2].append([len(data2[mail][2]) + 1, compra(str(data1[ans][0]), datetime.now(), data1[ans][1], True, True).asdict()])
-                                    file2.seek(0)
-                                    json.dump(data2, file2, indent = 4)
-                                    file2.truncate()
-                                    file2.close()
-                                    data1[ans][2] -= 1
-                                    file1.seek(0)
-                                    json.dump(data1, file1)
-                                    file1.truncate()
-                                    file1.close()
-                                    sock.sendall("Compra realizada con éxito!".encode())
-                                    print(f"[SERVIDOR] Cliente {data2[mail][1]} ha comprado {data1[ans][0]} por {data1[ans][1]}")
-                                    break
+                    if conf == "1": # si se confirma la compra...
+                        if data1[ans][2] > 0: # si hay stock...
+                            print("hasta aquí bien")                            
+                            with open(filepath2, "r+") as file2: # se abre el archivo asociado a los clientes
+                                print("hasta aquí bien")
+                                data2 = json.load(file2)
+                                data2[mail][2].append([len(data2[mail][2]) + 1, compra(str(data1[ans][0]), datetoDict(datetime.today()), data1[ans][1], True, True).asdict()]) # se guarda la compra en el historial del cliente
+                                file2.seek(0)
+                                json.dump(data2, file2, indent = 4)
+                                file2.truncate()
+                                file2.close()
+                                data1[ans][2] -= 1 # se reduce el stock luego de la compra
+                                file1.seek(0)
+                                json.dump(data1, file1)
+                                file1.truncate()
+                                file1.close()
+                                sock.sendall("Compra realizada con éxito!".encode())
+                                print(f"[SERVIDOR] Cliente {data2[mail][1]} ha comprado {data1[ans][0]} por {data1[ans][1]}")
+                                break
                         else:
-                            sock.sendall("No hay stock del artículo seleccionado. Lo sentimos :(".encode())
+                            sock.sendall("No hay stock del artículo seleccionado. Lo sentimos ".encode()) # no hay stock
                     elif conf == "0":
-                        sock.sendall("Compra cancelada.".encode())
-                        sock.sendall("¿Se te ofrece algo más?")
+                        sock.sendall("Compra cancelada.".encode()) # no se confirma la compra del artículo
+                        sock.sendall("¿Se te ofrece algo más?".encode())
                     else:
-                        sock.sendall("Ingrese una respuesta válida.".encode())
+                        sock.sendall("Ingrese una respuesta válida.".encode()) # se ingresa una respuesta inválida al confirmar la compra
                 else:
-                    sock.sendall("Ingrese un artículo válido.".encode())
+                    sock.sendall("Ingrese un artículo válido.".encode()) # se ingresa una id inválida al elegir un artículo
     return None
 
 def verHistorial(sock, filepath, mail):
@@ -109,7 +113,7 @@ def verHistorial(sock, filepath, mail):
             with open(filepath, "r") as file:
                 data = json.load(file)
                 hist = data[mail][2]
-                today = datetime.today()
+                today = datetoDict(datetime.today())
                 n = 0
                 for action in hist:
                     actDate = hist[action][1]["fecha"]
@@ -138,6 +142,5 @@ def determinarAccion(sock, x, filepath1, filepath2, mail):
                 break
         else:
             sock.sendall("Ingrese una acción válida.".encode())
-
 
 
